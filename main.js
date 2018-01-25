6e4c3c3afbd3ddd0701d32f317ca8b46eedb8ec4
sjcl <fruit@bebehp.com>
2018-01-25 16:35:55 +0900

    unresponsive crashed

--- main.js
+++ main.js
@@ -5,20 +5,49 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 
-let mainWindow = null;
+let win = null;
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     if (process.platform != 'darwin')
-      app.quit();
+        app.quit();
 });
 
-app.on('ready', function() {
+app.on('ready', function () {
+    win = new BrowserWindow({ width: 750, height: 675 });
+    win.loadURL('file://' + __dirname + '/main.html');
+    Menu.setApplicationMenu(null);
 
-  mainWindow = new BrowserWindow({width: 750, height: 675});
-  mainWindow.loadURL('file://' + __dirname + '/main.html');
-  Menu.setApplicationMenu(null);
+    win.on('unresponsive', function () {
+        const options = {
+            type: 'info',
+            title: '応答なし',
+            message: 'プロセスの応答がありません。',
+            buttons: ['再読み込み', '閉じる']
+        };
+        dialog.showMessageBox(options, function (index) {
+            if (index === 0)
+                win.reload();
+            else
+                win.close();
+        });
+    });
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    win.webContents.on('crashed', function () {
+        const options = {
+            type: 'info',
+            title: 'クラッシュ',
+            message: 'プロセスがクラッシュしました。申し訳ありません。',
+            buttons: ['再読み込み', '閉じる']
+        };
+        dialog.showMessageBox(options, function (index) {
+            if (index === 0)
+                win.reload();
+            else
+                win.close();
+        });
+    });
+
+    win.on('closed', function () {
+        win = null;
+    });
 });
