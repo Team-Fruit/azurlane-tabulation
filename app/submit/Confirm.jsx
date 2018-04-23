046488a04b141e2c6bb125b95c90c8bf4c128a8e
sjcl <jrhigasinihonn.0601@gmail.com>
2018-04-23 11:23:14 +0900

    figures

--- app/submit/Confirm.jsx
+++ app/submit/Confirm.jsx
@@ -13,12 +13,39 @@ export default class Confirm extends React.Component {
     }
 
     render() {
+        const { character, blueprint, count, boxtech } = this.props;
+        const itemList = [];
+        if (character)
+            itemList.push(
+                <figure className="confirmListItem" key="character">
+                    <img src={"./img/character/" + character + ".png"} />
+                    <figcaption>{character}</figcaption>
+                </figure>
+            );
+        if (blueprint)
+            itemList.push(
+                <figure className="confirmListItem" key="blueprint">
+                    <img src={"./img/blueprint/" + blueprint + ".png"} />
+                    <figcaption>{blueprint}</figcaption>
+                </figure>
+            );
+        if (boxtech)
+            itemList.push(
+                <figure className="confirmListItem" key="box">
+                    <img src={"./img/box/" + boxtech + ".png"} />
+                    <figcaption>{"装備箱T" + boxtech}</figcaption>
+                </figure>
+            );
+
         return (
             <div className="popup" onClick={this.handleClick}>
                 <div className="popupInner">
                     <div className="confirmList">
+                        <div className="confirmFigures">
+                            {itemList}
+                        </div>
                     </div>
-                    <img className="confirmButton" src="img/forward.png" width="120px" onClick={this.onForward} draggable="false" />
+                    <img className="confirmButton" src="img/forward.png" width="120px" onClick={this.props.onClose} draggable="false" />
                 </div>
             </div>
         );
