4d902241ee5699cd9261d6476e1e7d51a8fed5f5
sjcl <jrhigasinihonn.0601@gmail.com>
2018-05-14 16:00:20 +0900

    user auth

--- app/Spreadsheets.js
+++ app/Spreadsheets.js
@@ -1,19 +1,46 @@
 'use strict';
 
+const { app } = require('electron');
 const fs = require('fs');
+const path = require('path');
+const opn = require('opn');
 const { promisify } = require('util');
 const { google } = require('googleapis');
 const OAuth2Client = google.auth.OAuth2;
 
 const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+const credentialsPath = path.join(app.getPath('userData'), 'credentials.json');
 
+let userAuthRequired = false;
 let oAuth2Client;
+
+exports.userAuthRequired = () => {
+    return userAuthRequired;
+}
 
 exports.init = async () => {
     const credentials = JSON.parse(await readFileAsync('./app/client_secret.json', 'utf-8'));
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
-    oAuth2Client.setCredentials(JSON.parse(await readFileAsync('./app/credentials.json', 'utf-8')));
+
+    userAuthRequired = !fs.existsSync(credentialsPath);
+    if (userAuthRequired) {
+        const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: ['https://www.googleapis.com/auth/spreadsheets'],
+        });
+        opn(authUrl);
+    } else {
+        oAuth2Client.setCredentials(JSON.parse(await readFileAsync(credentialsPath, 'utf-8')));
+    }
+}
+
+exports.setCode = async (code) => {
+    const getToken = promisify(oAuth2Client.getToken);
+    const token = await getToken(code);
+    oAuth2Client.setCredentials(token);
+    await writeFileAsync(credentialsPath, JSON.stringify(token));
 }
 
 exports.submit = async (area, hard, character, blueprint, bpcount, boxtech, callback) => {
