76d11439237f57c020db5a3b3e45528d394d2d34
sjcl <jrhigasinihonn.0601@gmail.com>
2018-05-25 17:35:53 +0900

    autoUpdater

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // require('babel-polyfill');
 const electron = require('electron');
-const { app, BrowserWindow, Menu, globalShortcut } = electron;
+const { app, BrowserWindow, Menu, globalShortcut, autoUpdater, dialog } = electron;
 const path = require('path');
 const Spreadsheets = require('./app/Spreadsheets');
 const Splashscreen = require('@trodi/electron-splashscreen');
@@ -8,12 +8,12 @@ const Splashscreen = require('@trodi/electron-splashscreen');
 let win = null;
 let forceQuit = false;
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     if (process.platform != 'darwin')
         app.quit();
 });
 
-app.on('ready', function () {
+app.on('ready', () => {
     win = Splashscreen.initSplashScreen({
         windowOpts: { width: 750, height: 675 },
         templateUrl: path.join(__dirname, "./resources/icon.svg"),
@@ -67,9 +67,8 @@ app.on('ready', function () {
         }
     ];
     Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-    Spreadsheets.init(win);
 
-    win.on('close', function (e) {
+    win.on('close', (e) => {
         if (forceQuit || process.platform != 'darwin') {
             win = null;
         } else {
@@ -81,12 +80,43 @@ app.on('ready', function () {
     globalShortcut.register(process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Ctrl+Shift+I', () => {
         win.toggleDevTools();
     });
+
+    Spreadsheets.init(win);
+
+    const server = 'https://hazel-server-ezczncogdc.now.sh';
+    const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+    autoUpdater.setFeedURL(feed)
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+        const dialogOpts = {
+            type: 'info',
+            buttons: ['Restart', 'Later'],
+            title: 'Application Update',
+            message: process.platform === 'win32' ? releaseNotes : releaseName,
+            detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+        };
+
+        dialog.showMessageBox(dialogOpts, (response) => {
+            if (response === 0)
+                autoUpdater.quitAndInstall();
+        })
+    });
+    autoUpdater.on('error', (message) => {
+        console.error('There was a problem updating the application');
+        console.error(message);
+    });
+    autoUpdater.on('checking-for-update', () => {
+        console.log('checking-for-update');
+    });
+    setInterval((function callback() {
+        autoUpdater.checkForUpdates();
+        return callback;
+    }()), 600000);
 });
 
-app.on('before-quit', function () {
+app.on('before-quit', () => {
     forceQuit = true;
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
     win.show();
 });
