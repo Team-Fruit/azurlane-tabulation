0974d7892e1a66394c90be2f3e139332daf0223f
sjcl <jrhigasinihonn.0601@gmail.com>
2018-05-31 11:33:23 +0900

    refactor

--- app/Updater.js
+++ app/Updater.js
@@ -20,57 +20,51 @@ let remoteVersionData;
 let updateRequire = false;
 
 exports.updateRequired = async () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            localFiles.length = 0;
-            downloadQueue.length = 0;
-            deleteQueue.length = 0;
+    localFiles.length = 0;
+    downloadQueue.length = 0;
+    deleteQueue.length = 0;
 
-            if (!await fs.exists(baseDir))
-                await fs.mkdir(baseDir);
+    if (!await fs.exists(baseDir))
+        await fs.mkdir(baseDir);
 
-            remoteVersionData = await request({
-                url: versionFileUrl,
-                json: true
-            });
-
-            if (await fs.exists(versionFile)) {
-                localVersionData = JSON.parse(await fs.readFile(versionFile));
-                updateRequire = remoteVersionData.version > localVersionData.version;
-            } else
-                updateRequire = true;
-
-            if (updateRequire) {
-                const parse = (obj, base, array) => {
-                    Object.keys(obj).forEach((key) => {
-                        if (Array.isArray(obj[key]))
-                            obj[key].forEach((line) => array.push(base + key + '/' + line));
-                        else
-                            parse(obj[key], base + key + '/', array);
-                    });
-                }
-
-                const allFiles = [];
-                parse(remoteVersionData.data, '/', allFiles);
-                if (localVersionData)
-                    parse(localVersionData.data, '/', localFiles);
-
-                allFiles.forEach((line) => {
-                    if (!localFiles.includes(line))
-                        downloadQueue.push(line);
-                });
-                localFiles.forEach((line) => {
-                    if (!allFiles.includes(line))
-                        deleteQueue.push(line);
-                });
-
-                updateRequire = updateRequire || deleteQueue.length === 0;
-            }
-            resolve(updateRequire);
-        } catch (err) {
-            reject(err);
-        }
+    remoteVersionData = await request({
+        url: versionFileUrl,
+        json: true
     });
+
+    if (await fs.exists(versionFile)) {
+        localVersionData = JSON.parse(await fs.readFile(versionFile));
+        updateRequire = remoteVersionData.version > localVersionData.version;
+    } else
+        updateRequire = true;
+
+    if (updateRequire) {
+        const parse = (obj, base, array) => {
+            Object.keys(obj).forEach((key) => {
+                if (Array.isArray(obj[key]))
+                    obj[key].forEach((line) => array.push(base + key + '/' + line));
+                else
+                    parse(obj[key], base + key + '/', array);
+            });
+        }
+
+        const allFiles = [];
+        parse(remoteVersionData.data, '/', allFiles);
+        if (localVersionData)
+            parse(localVersionData.data, '/', localFiles);
+
+        allFiles.forEach((line) => {
+            if (!localFiles.includes(line))
+                downloadQueue.push(line);
+        });
+        localFiles.forEach((line) => {
+            if (!allFiles.includes(line))
+                deleteQueue.push(line);
+        });
+
+        updateRequire = updateRequire || deleteQueue.length === 0;
+    }
+    return updateRequire;
 }
 
 exports.getDownloadAmount = () => {
@@ -90,44 +84,30 @@ exports.deleteRequired = () => {
 }
 
 exports.download = async (onProgress) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            for (let line of downloadQueue) {
-                const file = path.join(baseDir, line);
-                await fse.ensureDir(path.dirname(file));
-                const body = await request({
-                    url: remoteDirUrl + encodeURI(line),
-                    encoding: null
-                });
-                await fs.writeFile(file, body, 'binary');
-                onProgress();
-            }
-            downloadQueue.length = 0;
-            if (deleteQueue.length === 0)
-                await onFinishUpdate();
-            resolve();
-        } catch (err) {
-            reject(err);
-        }
-    });
+    for (let line of downloadQueue) {
+        const file = path.join(baseDir, line);
+        await fse.ensureDir(path.dirname(file));
+        const body = await request({
+            url: remoteDirUrl + encodeURI(line),
+            encoding: null
+        });
+        await fs.writeFile(file, body, 'binary');
+        onProgress();
+    }
+    downloadQueue.length = 0;
+    if (deleteQueue.length === 0)
+        await onFinishUpdate();
 }
 
 exports.delete = async (onProgress) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            for (let line of deleteQueue) {
-                const file = path.join(baseDir, line);
-                await fs.unlink(file);
-                onProgress();
-            }
-            deleteQueue.length = 0;
-            if (downloadQueue.length === 0)
-                await onFinishUpdate();
-            resolve();
-        } catch (err) {
-            reject(err);
-        }
-    });
+    for (let line of deleteQueue) {
+        const file = path.join(baseDir, line);
+        await fs.unlink(file);
+        onProgress();
+    }
+    deleteQueue.length = 0;
+    if (downloadQueue.length === 0)
+        await onFinishUpdate();
 }
 
 const onFinishUpdate = async () => {
