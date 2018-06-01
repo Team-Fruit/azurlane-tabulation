e913aafdc760160649e4ee1d5153235e30cbd41c
sjcl <jrhigasinihonn.0601@gmail.com>
2018-06-01 16:03:32 +0900

    no longer hard codding chapter

--- app/chapter/Chapter.jsx
+++ app/chapter/Chapter.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Hard from './Hard.jsx';
-import Normal from './Normal.jsx';
+import { remote } from 'electron';
+import * as fs from 'fs';
+import path from 'path';
+
+const baseDir = path.join(remote.app.getPath('userData'), 'data/chapterdata');
 
 export default class Chapter extends React.Component {
     constructor(props) {
         super(props);
+
+        const chapter = JSON.parse(fs.readFileSync(path.join(baseDir, 'chapter.json')));
+        const sectionNames = {};
+        fs.readdirSync(baseDir).forEach((line) => {
+            if (line !== 'chapter.json')
+                sectionNames[line.slice(0, -5)] = JSON.parse(fs.readFileSync(path.join(baseDir, line))).name;
+        });
         this.state = {
-            isHard: false
+            isHard: false,
+            chapter,
+            sectionNames
         }
 
         this.onChoose = this.onChoose.bind(this);
@@ -21,14 +33,33 @@ export default class Chapter extends React.Component {
     }
 
     render() {
-        const isHard = this.state.isHard;
-        const component = isHard ? <Hard onChoose={this.onChoose} /> : <Normal onChoose={this.onChoose} />;
+        const components = [];
+        Object.keys(this.state.chapter).forEach((line, i) => {
+            const chapter = this.state.chapter[line];
+            if (!this.state.isHard || this.state.isHard === chapter.hard) {
+                const li = [];
+                chapter.sections.forEach((line, j) => {
+                    li.push(<li key={j}><a onClick={() => this.onChoose(line)}>{line + ' ' + this.state.sectionNames[line]}</a></li>);
+                });
+
+                components.push(
+                    <div key={i}>
+                        <label className="chapterLabel" htmlFor={"menu_bar" + i}>{chapter.label}</label>
+                        <input type="checkbox" id={"menu_bar" + i} className="accordion" />
+                        <ul className="accordionShow">
+                            {li}
+                        </ul>
+                    </div>
+                );
+            }
+        });
+
         return (
             <div>
-                <div className={isHard ? "chapter chapterHard" : "chapter chapterNormal"}>
-                    {component}
+                <div className={this.state.isHard ? "chapter chapterHard" : "chapter chapterNormal"}>
+                    {components}
                 </div>
-                <img className="bottomButton" src={isHard ? "../resources/img/normal.png" : "../resources/img/hard.png"} width="150px" onClick={() => this.toggleHard()} draggable="false" />
+                <img className="bottomButton" src={this.stateisHard ? "../resources/img/normal.png" : "../resources/img/hard.png"} width="150px" onClick={() => this.toggleHard()} draggable="false" />
             </div>
         );
     }
