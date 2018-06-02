94ed41dd39ffa2aa61c36fa17f452071e6e92e20
sjcl <fruit@bebehp.com>
2018-06-02 23:07:13 +0900

    refactor

--- app/Spreadsheets.js
+++ app/Spreadsheets.js
@@ -1,64 +1,41 @@
 'use strict';
 
 const { app, shell } = require('electron');
-const fs = require('fs');
+const fs = require('mz/fs');
 const path = require('path');
-const { promisify } = require('util');
 const { google } = require('googleapis');
 const OAuth2Client = google.auth.OAuth2;
 const prompt = require('electron-prompt');
 
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
 const credentialsPath = path.join(app.getPath('userData'), 'credentials.json');
 const rangeReg = /\d+/;
 const sheetIds = new Map();
+
+const gold = {
+    red: 1,
+    green: 0.851,
+    blue: 0.4
+}
+const purple = {
+    red: 0.5569,
+    green: 0.4863,
+    blue: 0.7686
+}
+const blue = {
+    red: 0.4275,
+    green: 0.6235,
+    blue: 0.9216
+}
 const colors = {
-    gold: {
-        red: 1,
-        green: 0.851,
-        blue: 0.4
-    },
-    purple: {
-        red: 0.5569,
-        green: 0.4863,
-        blue: 0.7686
-    },
-    blue: {
-        red: 0.4275,
-        green: 0.6235,
-        blue: 0.9216
-    },
-    ssr: {
-        red: 1,
-        green: 0.851,
-        blue: 0.4
-    },
-    sr: {
-        red: 0.5569,
-        green: 0.4863,
-        blue: 0.7686
-    },
-    r: {
-        red: 0.4275,
-        green: 0.6235,
-        blue: 0.9216
-    },
-    3: {
-        red: 1,
-        green: 0.851,
-        blue: 0.4
-    },
-    2: {
-        red: 0.5569,
-        green: 0.4863,
-        blue: 0.7686
-    },
-    1: {
-        red: 0.4275,
-        green: 0.6235,
-        blue: 0.9216
-    },
+    gold,
+    ssr: gold,
+    3: gold,
+    purple,
+    sr: purple,
+    2: purple,
+    blue,
+    r: blue,
+    1: blue
 };
 
 let mainWindow;
@@ -66,36 +43,33 @@ let oAuth2Client;
 
 exports.init = async (window) => {
     mainWindow = window;
-    const credentials = JSON.parse(await readFileAsync(path.join(__dirname, '../resources/client_secret.json'), 'utf-8'));
+    const credentials = JSON.parse(await fs.readFile(path.join(__dirname, '../resources/client_secret.json')));
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
 
-    const userAuthRequired = !fs.existsSync(credentialsPath);
-    if (userAuthRequired) {
+    if (!await fs.exists(credentialsPath)) {
         const authUrl = oAuth2Client.generateAuthUrl({
             access_type: 'offline',
             scope: ['https://www.googleapis.com/auth/spreadsheets'],
         });
         shell.openExternal(authUrl);
-        prompt({
+        this.setCode(await prompt({
             title: "Google認証",
             label: "認証コードを入力してください",
             alwaysOnTop: true
-        }).then((r) => {
-            this.setCode(r);
-        });
+        }));
     } else {
-        oAuth2Client.setCredentials(JSON.parse(await readFileAsync(credentialsPath, 'utf-8')));
+        oAuth2Client.setCredentials(JSON.parse(await fs.readFile(credentialsPath)));
         fetchSheetIds();
     }
 }
 
-exports.setCode = async (code) => {
-    oAuth2Client.getToken(code, (err, token) => {
+exports.setCode = (code) => {
+    oAuth2Client.getToken(code, async (err, token) => {
         if (token) {
             oAuth2Client.setCredentials(token);
-            fs.writeFileSync(credentialsPath, JSON.stringify(token));
-            fetchSheetIds();
+            await fs.writeFile(credentialsPath, JSON.stringify(token));
+            await fetchSheetIds();
         }
     });
 }
